1038131f36d56ebd45dcd3b10360682de5520425
Pedro Victor <146785947+PedroVII@users.noreply.github.com>
2024-07-15 11:01:24 -0300

    Mundo1-exercicio1 feito

---
+++ mundo1/nivel1/calculadora.py
@@ -0,0 +1,6 @@
+a = 20
+b = 10
+print("Resultado da adição: " + str(a + b))
+print("Resultado da subtração: " + str(a - b))
+print("Resultado da multiplicação: " + str(a * b))
+print("Resultado da divisão: " + str(a / b))
---
+++ mundo1/nivel1/teste.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+print(1 + 1)
+print (7 - 2)
+print (5 * 10)
+print (10 / 2)
